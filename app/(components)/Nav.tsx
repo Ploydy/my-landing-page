21fa38e962819eaa6fddec102e8feed3523e84e3
Ploydy <Lfecarballo20@gmail.com>
2023-11-19 00:28:11 +0800

    header management

--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -1,7 +1,12 @@
 const Nav = () => {
   return (
-    <nav className="flex justify-between bg-nav p-4">
-      Header
+    <nav className="flex justify-between rounded bg-slate-600 p-4">
+      <div className="flex items-center space-x-5">
+        <div>home</div>
+        <div>header</div>
+        <div>header</div>
+        <div>header</div>
+      </div>
     </nav>
   );
 };
