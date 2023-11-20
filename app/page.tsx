b0048d8d2d3a07ba2f809411bf75833ff0c40886
Ploydy <Lfecarballo20@gmail.com>
2023-11-20 16:24:22 +0800

    3rd section ready

--- app/page.tsx
+++ app/page.tsx
@@ -164,7 +164,9 @@ const Dashboard = () => {
               <div className="overflow-hidden aspect-w-1 aspect-h-1">
                 <Image
                   className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125"
-                  src={"https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/item-cards/4/product-2.png"}
+                  src={
+                    "https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/item-cards/4/product-2.png"
+                  }
                   alt="Product image"
                   height={500}
                   width={500}
@@ -237,7 +239,9 @@ const Dashboard = () => {
               <div className="overflow-hidden aspect-w-1 aspect-h-1">
                 <Image
                   className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125"
-                  src={"https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/item-cards/4/product-3.png"}
+                  src={
+                    "https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/item-cards/4/product-3.png"
+                  }
                   alt="Product image"
                   height={500}
                   width={500}
@@ -319,7 +323,9 @@ const Dashboard = () => {
               <div className="overflow-hidden aspect-w-1 aspect-h-1">
                 <Image
                   className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125"
-                  src={"https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/item-cards/4/product-4.png"}
+                  src={
+                    "https://cdn.rareblocks.xyz/collection/clarity-ecommerce/images/item-cards/4/product-4.png"
+                  }
                   alt="Product image"
                   height={500}
                   width={500}
@@ -391,13 +397,268 @@ const Dashboard = () => {
         </div>
       </section>
 
+      <section className="py-12 bg-white sm:py-16 lg:py-20">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">
+              Our Investors & Board of Directors
+            </h2>
+          </div>
 
+          <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-2 md:mt-20 gap-x-8 md:grid-cols-4 gap-y-12 lg:gap-x-16 xl:gap-x-20">
+            <div>
+              <Image
+                className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-1.png"
+                alt="Investor and Board of Director image 1"
+                height={500}
+                width={500}
+              />
+              <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">
+                Jerome Bell
+              </p>
+              <p className="mt-2 text-base font-normal text-gray-600 font-pj">
+                Co founder, Chairman, Executive Director
+              </p>
+            </div>
 
+            <div>
+              <Image
+                className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-2.png"
+                alt="Investor and Board of Director image 2"
+                height={500}
+                width={500}
+              />
+              <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">
+                Jerome Bell
+              </p>
+              <p className="mt-2 text-base font-normal text-gray-600 font-pj">
+                Co founder, Chairman, Executive Director
+              </p>
+            </div>
 
+            <div>
+              <Image
+                className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-3.png"
+                alt="Investor and Board of Director image 3"
+                height={500}
+                width={500}
+              />
+              <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">
+                Jerome Bell
+              </p>
+              <p className="mt-2 text-base font-normal text-gray-600 font-pj">
+                Co founder, Chairman, Executive Director
+              </p>
+            </div>
 
+            <div>
+              <Image
+                className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-4.png"
+                alt="Investor and Board of Director image 1"
+                height={500}
+                width={500}
+              />
+              <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">
+                Jerome Bell
+              </p>
+              <p className="mt-2 text-base font-normal text-gray-600 font-pj">
+                Co founder, Chairman, Executive Director
+              </p>
+            </div>
+          </div>
 
+          <div className="mt-12 sm:mt-16">
+            <svg
+              className="w-auto h-4 mx-auto text-gray-300"
+              viewBox="0 0 172 16"
+              fill="none"
+              stroke="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 11 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 46 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 81 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 116 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 151 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 18 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 53 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 88 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 123 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 158 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 25 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 60 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 95 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 130 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 165 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 32 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 67 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 102 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 137 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 172 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 39 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 74 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 109 1)"
+              />
+              <line
+                y1="-0.5"
+                x2="18.0278"
+                y2="-0.5"
+                transform="matrix(-0.5547 0.83205 0.83205 0.5547 144 1)"
+              />
+            </svg>
+          </div>
 
-      
+          <div className="max-w-3xl mx-auto mt-12 space-y-8 sm:space-y-0 sm:flex sm:items-center sm:justify-center sm:mt-16 sm:gap-x-16">
+            <div>
+              <Image
+                className="w-auto mx-auto h-11"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/logo-waverio.svg"
+                alt="Logo image 1"
+                width={500}
+                height={500}
+              />
+            </div>
+
+            <div>
+              <Image
+                className="w-auto mx-auto h-11"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/logo-squarestone.svg"
+                alt="Logo image 2"
+                width={500}
+                height={500}
+              />
+            </div>
+
+            <div>
+              <Image
+                className="w-auto mx-auto h-11"
+                src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/logo-creaty.svg"
+                alt="Logo image 3"
+                width={500}
+                height={500}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // start of hero image
       {
         protocol: 'https',
         hostname: 'cdn.rareblocks.xyz',
         pathname: '/collection/celebration/images/hero/1/hero-img.png',
       },
+      // end of hero image
+
+      // start of products image
       {
         protocol: 'https',
         hostname: 'cdn.rareblocks.xyz',
@@ -26,7 +30,31 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.rareblocks.xyz',
         pathname: '/collection/clarity-ecommerce/images/item-cards/4/product-4.png',
-      }
+      },
+      // end of products image
+      
+      // start of directors image
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity/images/team/1/team-member-1.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity/images/team/1/team-member-2.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity/images/team/1/team-member-3.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity/images/team/1/team-member-4.png',
+      },
+      // end of directors image
     ]
   }
 }
