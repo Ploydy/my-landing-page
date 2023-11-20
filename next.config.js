19e9000b2664480f4e0cc39fae126d07608b4344
Ploydy <Lfecarballo20@gmail.com>
2023-11-20 16:11:56 +0800

    2nd section ready

--- next.config.js
+++ next.config.js
@@ -6,6 +6,26 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.rareblocks.xyz',
         pathname: '/collection/celebration/images/hero/1/hero-img.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity-ecommerce/images/item-cards/4/product-1.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity-ecommerce/images/item-cards/4/product-2.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity-ecommerce/images/item-cards/4/product-3.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.rareblocks.xyz',
+        pathname: '/collection/clarity-ecommerce/images/item-cards/4/product-4.png',
       }
     ]
   }
